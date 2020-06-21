e53b7189f23e9b9802a643248691b80c08e17a88
Rashadat Seyidzada <rashadat.seyidzadeh@gmail.com>
2020-06-22 01:12:05 +0400

    first commit

---
+++ src/test/java/com/developer/SpringWebApp2060/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developer.SpringWebApp2060;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
